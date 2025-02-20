d68ebfb88322f7558ec707e344a35503e7306d18
Mateusz Zakarczemny <mzakarczemny@caesars.com>
2025-02-20 16:47:40 +0100

    Fix duplicated spans

--- src/main/scala/com/matzz/OTELInterceptor.scala
+++ src/main/scala/com/matzz/OTELInterceptor.scala
@@ -1,23 +1,23 @@
 package com.matzz
 
 import io.opentelemetry.api.GlobalOpenTelemetry
-import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.api.trace.{Span, Tracer}
 import sttp.tapir.server.metrics.{EndpointMetric, Metric}
 
 import scala.concurrent.Future
 
 object OTELInterceptor {
-  private lazy val tracer: Tracer = GlobalOpenTelemetry
-    .get()
-    .getTracerProvider
-    .get("scope.name", "scope.version")
-
-  lazy val otelMetric: Metric[Future, Tracer] =
+//  private lazy val tracer: Tracer = GlobalOpenTelemetry
+//    .get()
+//    .getTracerProvider
+//    .get("scope.name", "scope.version")
+//
+  lazy val otelMetric: Metric[Future, Unit] =
     Metric(
-      metric = tracer,
+      metric = (),
       onRequest = (serverRequest, agg, me) =>
         me.eval {
-          val span = agg.spanBuilder("unknown").startSpan()
+          val span = Span.current()
 
 
           EndpointMetric(
@@ -27,17 +27,6 @@ object OTELInterceptor {
                 val method = endpoint.method.map(_.method).getOrElse("")
                 span.updateName(s"${method} ${path}")
               }
-            ),
-            onResponseHeaders = None,
-            onResponseBody = Some((ep, response) =>
-              me.eval {
-                span.end()
-              },
-            ),
-            onException = Some((ep, throwable) =>
-              me.eval {
-                span.end()
-              },
             )
           )
         },
